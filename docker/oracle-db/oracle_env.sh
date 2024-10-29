e53b4161eb509ade05b243fbd8694541fb19231c
Tobotobo <tobotobo.channel@gmail.com>
2024-10-30 02:41:35 +0900

    試行錯誤中

--- docker/oracle-db/oracle_env.sh
+++ docker/oracle-db/oracle_env.sh
@@ -3,3 +3,5 @@ export ORACLE_HOME=/opt/oracle/product/18c/dbhomeXE
 export ORACLE_SID=XE
 export PATH=PATH:/opt/oracle/product/18c/dbhomeXE/bin
 export NLS_LANG=AMERICAN_AMERICA.AL32UTF8
+export NLS_DATE_FORMAT="YYYY-MM-DD HH24:MI:SS"
+export NLS_TIMESTAMP_FORMAT="YYYY-MM-DD HH24:MI:SS.FF"
